90dbf079f251fc5493743b7efc184d827a923b66
Paul Mospan <pavel.mospan@yahoo.com>
2019-08-24 00:16:31 +0300

    scrapping quotes with forloop and hence recieving more clear and systematic outcome

--- quotes/quotes/spiders/quotes_spider.py
+++ quotes/quotes/spiders/quotes_spider.py
@@ -11,15 +11,17 @@ class QuotesSpider(scrapy.Spider):
     # response contains a source code of the web page we are scrapping (from start_url)
     def parse(self, response):
         all_div_quotes = response.css("div.quote")
-        title = all_div_quotes.css("span.text::text").extract()
-        author = all_div_quotes.css(".author::text").extract()
-        tag = all_div_quotes.css(".tag::text").extract()
 
-        # yield = return
-        # yield works with generator
-        yield {
-            'title': title,
-            'author': author,
-            'tag': tag
-        }
+        for quote in all_div_quotes:
+            title = quote.css("span.text::text").extract()
+            author = quote.css(".author::text").extract()
+            tag = quote.css(".tag::text").extract()
+
+            # yield = return
+            # yield works with generator
+            yield {
+                'title': title,
+                'author': author,
+                'tag': tag
+            }
 
